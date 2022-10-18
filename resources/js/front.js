d6ca8051dacbfeb77e2807a8d0c4832c22ded4ad
ValerioGc <valerio.gc1@gmail.com>
2022-10-18 18:10:26 +0200

    Creazione router e importazione

--- resources/js/front.js
+++ resources/js/front.js
@@ -2,10 +2,12 @@ window.axios = require('axios');
 
 import Vue from 'vue';
 import App from './views/App';
+import router from './router.js';
+
 
 const app = new Vue({
     el: '#root',
-    render: h => h(App)
+    render: h => h(App), router
 });
 
 
